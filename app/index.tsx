d5df1dfadbbc746b18b5a622b9e0110ee455313b
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-08-13 00:39:05 +0200

    Cleanup project

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import{ Image, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className='flex-1 justify-center'>
+      <Text>Home!</Text>
+    </View>
+  );
+}
