9f2c570cf90c6539a121eecba6fedaaa45844c55
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-08-12 22:28:24 +0200

    Add Standard Trainings

---
+++ constants/StandardTrainings.ts
@@ -0,0 +1,25 @@
+export const StandardTrainings = [
+    {
+        "round-duration": 30,
+        "rounds": 3,
+        "rest-duration": 15,
+        "name": "Short training",
+        "min-dificulty": 1,
+        "max-dificulty": 2,
+    },
+    {
+        "round-duration": 45,
+        "rounds": 4,
+        "rest-duration": 15,
+        "name": "Medium training",
+        "min-dificulty": 1,
+        "max-dificulty": 3,
+    },
+    {
+        "round-duration": 60,
+        "rounds": 5,
+        "rest-duration": 20,
+        "name": "Long training",
+        "min-dificulty": 2,
+        "max-dificulty": 3,
+    },]
