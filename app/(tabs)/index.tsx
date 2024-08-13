9fa07eea9ab64faadb00f4c7085ee767e2909e07
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-08-13 12:33:01 +0200

    Move index to (tabs)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ function formatTechnics(technics: number[]): string {
     return rounds * (roundDuration + restDuration) - restDuration;
   }
 
-export default function HomeScreen() {
+export default function Home() {
   return (
     <View className='flex-1 bg-gray-800'>
         <SafeAreaView className='flex-1 bg-gray-800'>
@@ -27,22 +27,22 @@ export default function HomeScreen() {
                 StandardTrainings.map((training) => (
                     <Collapsible title={training.name} key={training.name}>
                         <>
-                        <Text className='flex text-xl text-blue-300 text-left py-2'>Duration: {calculateTrainingDuration(
+                        <Text className='flex text text-blue-300 text-left py-2'>Duration: {calculateTrainingDuration(
                                 training.rounds,
                                 training['round-duration'],
                                 training['rest-duration'],
                             )} seconds
                         </Text>
-                        <Text className='flex text-xl text-blue-300 text-left py-2'>
+                        <Text className='flex text text-blue-300 text-left py-2'>
                             Diffculty: {training['min-dificulty']} - {training['max-dificulty']}
                         </Text>  
-                        <Text className='flex text-xl text-blue-300 text-left py-2'>
+                        <Text className='flex text text-blue-300 text-left py-2'>
                             Number of rounds: {training.rounds}
                         </Text>
-                        <Text className='flex text-xl text-blue-300 text-left py-2'>
+                        <Text className='flex text text-blue-300 text-left py-2'>
                             Round duration: {training['round-duration']} seconds
                         </Text>
-                        <Text className='flex text-xl text-blue-300 text-left py-2'>
+                        <Text className='flex text text-blue-300 text-left py-2'>
                             Rest duration: {training['rest-duration']} seconds
                         </Text>
                         <Button title={`Start ${training.name}`} onPress={() => {}} />
