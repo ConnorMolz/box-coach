b684b431dad22b4122ced146865666987358f620
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-08-13 12:33:30 +0200

    Add Combo page (is currently empty)

---
+++ app/(tabs)/combos.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const combos = () => {
+  return (
+    <View>
+      <Text>combos</Text>
+    </View>
+  )
+}
+
+export default combos
